8d23c012032b38a2cc59e3f87d58ffa69e38e42f
Roland Tritsch <roland@tritsch.org>
2018-12-03 10:32:31 +0000

    Day02: Try to go from n*m passes to n+m passes. Core dumps with ExitFailure (-9).

--- app/Day02/Part2.hs
+++ app/Day02/Part2.hs
@@ -7,15 +7,15 @@ import Control.Exception (evaluate)
 import Day02
 
 -- | compare every boxid with every boxid and find
--- out what the difference is and what is common.
+-- out what the difference is (and what is common :)).
 diff :: [BoxId] -> [(BoxId, BoxId, String, String)]
-diff boxIds = concatMap diffId boxIds where
-  diffId bid = map diffId' boxIds where
-    diffId' bid' = (bid, bid', difference', common') where
-      difference' = concat $ zipWith notSame bid bid' where
-        notSame c0 c1 = if c0 /= c1 then [c0] else ""
-      common' = concat $ zipWith same bid bid' where
-        same c0 c1 = if c0 == c1 then [c0] else ""
+diff (bid:rest) = concatMap diffId' rest where
+  diffId' bid' = [(bid, bid', difference', common')] ++ diff rest where
+    difference' = concat $ zipWith notSame bid bid' where
+      notSame c0 c1 = if c0 /= c1 then [c0] else ""
+    common' = concat $ zipWith same bid bid' where
+      same c0 c1 = if c0 == c1 then [c0] else ""
+diff [] = []
 
 -- | solve the puzzle
 solve :: [BoxId] -> String
