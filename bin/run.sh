bc8fd7b4021ebe3363363f61a04a97d2e5e16a2b
Roland Tritsch <roland@tritsch.org>
2018-12-04 19:54:47 +0000

    Remove the uberjar from the run (to make it compile faster).

--- bin/run.sh
+++ bin/run.sh
@@ -11,16 +11,15 @@ then
     stack test :${target} --test-arguments "--match Part${PART}"
   fi
 else
+  ETA_JAVA_ARGS="-Xss1024M -Xmx4096M"
   eta_version=$(etlas exec eta -- --version | cut -d, -f2 | cut -d\  -f3 | sed s/b/./g)
 
   if [ "${TYPE}" = "EXE" ]
   then
     target=haskell-aoc-day${DAY}p${PART}
-    etlas build --enable-uberjar-mode ${target}
-    java -Xss1024M -Xmx4096M -jar dist/build/eta-${eta_version}/haskell-aoc-0.1.0.0/x/${target}/build/${target}/${target}.jar
-else
+    etlas build ${target} && etlas run ${target}
+  else
     target=haskell-aoc-test-day${DAY}
-    etlas build --enable-uberjar-mode ${target}
-    java -Xss1024M -Xmx4096M -jar dist/build/eta-${eta_version}/haskell-aoc-0.1.0.0/t/${target}/build/${target}/${target}.jar --match "Part${PART}"
+    etlas build ${target} && etlas run ${target} -- --match "Part${PART}"
   fi
 fi
