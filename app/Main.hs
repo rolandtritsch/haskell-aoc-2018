ab68e7c3bffc3f315a8cdc1b14e007dee24c7968
Roland Tritsch <roland@tritsch.org>
2018-12-20 08:09:29 +0000

    Make exe run again (on eta; by excluding Day14 Part2).

--- app/Main.hs
+++ app/Main.hs
@@ -27,11 +27,11 @@ import qualified Day12.Part2 as D12P2
 import qualified Day13.Part1 as D13P1
 import qualified Day13.Part2 as D13P2
 import qualified Day14.Part1 as D14P1
-import qualified Day14.Part2 as D14P2
+--import qualified Day14.Part2 as D14P2
 import qualified Day15.Part1 as D15P1
 import qualified Day15.Part2 as D15P2
---import qualified Day16.Part1 as D16P1
---import qualified Day16.Part2 as D16P2
+import qualified Day16.Part1 as D16P1
+import qualified Day16.Part2 as D16P2
 
 main :: IO ()
 main = do
@@ -68,8 +68,10 @@ main = do
   D13P1.main
   D13P2.main
   D14P1.main
-  D14P2.main
+  --D14P2.main
+  -- runs out of mem for/on eta.
+  putStrLn "TODO - Day14: Dummy: Part2: dummy -> (0, 0)"
   D15P1.main
   D15P2.main
-  --D16P1.main
-  --D16P2.main
+  D16P1.main
+  D16P2.main
